82174115fe7533b84335795aaed1aa1817ba208f
Dominik Boehler <dominik.boehler@gmx.net>
2022-11-22 07:55:10 +0100

    cleanup before session 5

--- 22S_mock_exam_2/HI-B-2 Software Development (SS22)-mock exam 2-466534/Madalina Lupacescu_1706595_assignsubmission_file_/python1.py
+++
@@ -1,5 +0,0 @@
-#Complex data
-# tuple - immutable sequence of objects
-# list - mutable sequence of objects
-# set - every element is immutable
-# All the objects are part of the heterogeneous data srtucture, which mean they are not the same type of data
