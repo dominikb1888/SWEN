b35778b0003dcbf503a23241db2d9f9c2bb8d401
Dominik Boehler <dominik.boehler@gmx.net>
2023-04-24 15:40:32 +0200

    added alternative init method with @classmethod decorator. Now we can pass a filepath to create the MarriageStats class

--- code/Session_04/marriage_stats.py
+++ code/Session_04/marriage_stats.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import datetime
+import csv
 from typing import Any
 
 @dataclass
@@ -11,11 +12,32 @@ class MarriageRecord:
 
 @dataclass
 class MarriageStats:
-    data: list[Any]
+    data: list[MarriageRecord] = None
 
     def __post_init__(self):
-        for i, item in enumerate(self.data):
-            self.data[i] = MarriageRecord(*item)
+        if self.data:
+            for i, item in enumerate(self.data):
+                if not isinstance(item, MarriageRecord):
+                    self.data[i] = MarriageRecord(*item)
+
+    # The classmethod decorator allows alternative init functions, e.g.
+    # with a csv file instead of a list as an input. It will take the file
+    # pre-process it and create the object
+    @classmethod
+    def from_csv(cls, filepath):
+        with open(filepath) as f:
+            csv_data = csv.reader(f, delimiter=',')
+            data = []
+            next(csv_data)
+            for row in csv_data:
+                row[0] = int(row[0])
+                row[1] = str(row[1])
+                row[2] = int(row[2])
+                year, month = row[3].split('-')
+                row[3] = datetime.date(year=int(year), month=int(month), day=1)
+                data.append(MarriageRecord(*row))
+
+        return cls(data)
 
     def average_licenses(self, key: str) -> int:
          licenses_issued = [row.amount for row in self.data if row.center == key]
@@ -24,25 +46,23 @@ class MarriageStats:
          else:
              return sum(licenses_issued) / len(licenses_issued)
 
-    def get_averages(self):
+    def get_averages(self) -> dict:
         centers = {record.center for record in self.data}
         return {key: self.average_licenses(key) for key in centers}
 
 
 if __name__ == '__main__':
-    marriage_data = [
-     [1657, 'ET', 80, datetime.date(2011, 1, 1)],
-     [1658, 'NY', 136, datetime.date(2011, 1, 1)],
-     [1659, 'SC', 159, datetime.date(2011, 1, 1)],
-     [1660, 'TO', 367, datetime.date(2011, 1, 1)],
-     [1661, 'ET', 109, datetime.date(2011, 2, 1)],
-     [1662, 'NY', 150, datetime.date(2011, 2, 1)],
-     [1663, 'SC', 154, datetime.date(2011, 2, 1)],
-     [1664, 'TO', 383, datetime.date(2011, 2, 1)]
-    ]
+    # marriage_data = [
+    #  [1657, 'ET', 80, datetime.date(2011, 1, 1)],
+    #  [1658, 'NY', 136, datetime.date(2011, 1, 1)],
+    #  [1659, 'SC', 159, datetime.date(2011, 1, 1)],
+    #  [1660, 'TO', 367, datetime.date(2011, 1, 1)],
+    #  [1661, 'ET', 109, datetime.date(2011, 2, 1)],
+    #  [1662, 'NY', 150, datetime.date(2011, 2, 1)],
+    #  [1663, 'SC', 154, datetime.date(2011, 2, 1)],
+    #  [1664, 'TO', 383, datetime.date(2011, 2, 1)]
+    # ]
 
-    stats = MarriageStats(marriage_data)
-
-
+    stats = MarriageStats().from_csv("Marriage Licence Statistics Data.csv")
     print(stats.get_averages())
 
