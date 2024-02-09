3e49e40b96deea81c18d1433419373e25767ac0e
Dominik Boehler <dominik.boehler@gmx.net>
2024-02-09 11:43:16 +0100

    switching to rust

--- code/Session_06/quadratic.py
+++
@@ -1,8 +0,0 @@
-def print_pairs(n: int, start: int = None, end: int = None) -> None:
-    """Print all combinations of pairs of the first n natural numbers."""
-    rlist = list(range(0,n))
-    listc = [(i, rlist) for i in range(0, n)]
-    print(listc[start:end])
-
-if __name__ == "__main__":
-    print_pairs(10000, 9900, 10000)
