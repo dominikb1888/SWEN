2abd873522b35244c38d9cfb090096f61b8509b9
Dominik Boehler <dominik.boehler@gmx.net>
2026-03-17 04:35:57 -0700

    new and push implemented

--- s01/firstlist26/src/first.rs
+++ s01/firstlist26/src/first.rs
@@ -1,10 +1,35 @@
-struct Node {
-    elem: i32,
-    next: List,
+use std::mem;
+
+
+#[derive(Debug)]
+pub struct List {
+    head: Link,
 }
 
-pub enum List {
+#[derive(Debug)]
+enum Link {
     Empty,
     More(Box<Node>),
 }
 
+#[derive(Debug)]
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        
+        let new_node = Box::new( Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+
+        self.head = Link::More(new_node);
+    }
+}
--- s01/firstlist26/src/main.rs
+++ s01/firstlist26/src/main.rs
@@ -2,5 +2,10 @@ use firstlist26::first::List;
 
 
 fn main() {
-    todo!();
+    let mut list = List::new();
+    println!("{:?}", list);
+    list.push(32);
+    println!("{:?}", list);
+    list.push(40);
+    println!("{:?}", list);
 }
